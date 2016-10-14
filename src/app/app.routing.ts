b179d51468f4227cdbd79d147e3530021993d56d
Jussi Kinnula <jussi.kinnula@frantic.com>
2016-10-14 14:18:47 +0300

    Convention change (Angular Styleguide): Remove '+' from lazy loaded route module paths

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,9 +3,9 @@ import { Routes } from "@angular/router";
 declare var System: any;
 
 export const AppRoutes: Routes = [
-    { path: "", pathMatch: "full", loadChildren: "./+frontpage/index#FrontpageModule" },
-    { path: "organizers", loadChildren: "./+organizers/index#OrganizersModule" },
-    { path: "contact", loadChildren: "./+contact/index#ContactModule" },
-    { path: "404", loadChildren: "./+pagenotfound/index#PageNotFoundModule" },
+    { path: "", pathMatch: "full", loadChildren: "./frontpage/index#FrontpageModule" },
+    { path: "organizers", loadChildren: "./organizers/index#OrganizersModule" },
+    { path: "contact", loadChildren: "./contact/index#ContactModule" },
+    { path: "404", loadChildren: "./pagenotfound/index#PageNotFoundModule" },
     { path: "**", pathMatch: "full", redirectTo: "/404" }
 ];
