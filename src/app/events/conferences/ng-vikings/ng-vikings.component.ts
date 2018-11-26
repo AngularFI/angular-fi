43893baad6fdf26139ea9b2c76cb1d2467be72d8
Unknown <richard@richardfarkas.net>
2018-11-26 04:35:28 +0200

    Conference 2019 microsite v0.1

---
+++ src/app/events/conferences/ng-vikings/ng-vikings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-vikings',
+  templateUrl: './ng-vikings.component.html',
+  styleUrls: ['./ng-vikings.component.css']
+})
+export class NgVikingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
